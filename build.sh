0652f6f891033f08fad1e6ff8a8bafe221f54876
Jiao Ye <uframer@gmail.com>
2015-12-30 16:43:16 +0800

    Set indention width to 4 spaces

--- build.sh
+++ build.sh
@@ -39,11 +39,11 @@ fi
 cd ${target_dir}
 linux_builder_script=linux_builder_script
 if [ -d ${linux_builder_script} ] ; then
-  cd ${linux_builder_script}
-  git pull
+    cd ${linux_builder_script}
+    git pull
 else
-  git clone https://github.com/uframer/armv7-multiplatform.git ${linux_builder_script}
-  cd ${linux_builder_script}
+    git clone https://github.com/uframer/armv7-multiplatform.git ${linux_builder_script}
+    cd ${linux_builder_script}
 fi
 
 # Checkout v4.3.x branch
@@ -52,24 +52,24 @@ git checkout remotes/origin/v4.3.x -b build
 
 need_rebuild_linux=0
 if [ ! -f deploy/4.3.3-armv7-x1.zImage ] ; then
-  need_rebuild_linux=1
+    need_rebuild_linux=1
 fi
 if [ ! -f deploy/4.3.3-armv7-x1-dtbs.tar.gz ] ; then
-  need_rebuild_linux=1
+    need_rebuild_linux=1
 fi
 if [ ! -f deploy/4.3.3-armv7-x1-firmware.tar.gz ] ; then
-  need_rebuild_linux=1
+    need_rebuild_linux=1
 fi
 if [ ! -f deploy/4.3.3-armv7-x1-modules.tar.gz ] ; then
-  need_rebuild_linux=1
+    need_rebuild_linux=1
 fi
 if [ ! -f deploy/config-4.3.3-armv7-x1 ] ; then
-  need_rebuild_linux=1
+    need_rebuild_linux=1
 fi
 
 if [ ${need_rebuild_linux} == "1" ] ; then
-  echo "Need to build Linux kernel"
-  AUTO_BUILD=1 LINUX_GIT="${local_linux_source}" ./build_kernel.sh
+    echo "Need to build Linux kernel"
+    AUTO_BUILD=1 LINUX_GIT="${local_linux_source}" ./build_kernel.sh
 else
   echo "Linux kernel was built before, skip this phase"
 fi
@@ -91,22 +91,22 @@ mkdir -p ${ubuntu_core_dir}
 cd ${ubuntu_core_dir}
 rootfs_archive="ubuntu-core-15.04-core-armhf.tar.gz"
 if [ ! -f ${rootfs_archive} ] ; then
-  wget http://cdimage.ubuntu.com/ubuntu-core/releases/15.04/release/SHA1SUMS
-  wget http://cdimage.ubuntu.com/ubuntu-core/releases/15.04/release/${rootfs_archive}
+    wget http://cdimage.ubuntu.com/ubuntu-core/releases/15.04/release/SHA1SUMS
+    wget http://cdimage.ubuntu.com/ubuntu-core/releases/15.04/release/${rootfs_archive}
 fi
 # Check SHA1
 grep armhf SHA1SUMS | sha1sum -c
 if [ $? != "0" ] ; then
-  echo "rootfs verification failed, please delete intermediate file and retry"
-  exit 1
+    echo "rootfs verification failed, please delete intermediate file and retry"
+    exit 1
 fi
 
 # Prepare target disk
 cd ${target_dir}
 file ${target_device} | grep block
 if [ $? != "0" ] ; then
-  echo "Target device ${target_device} does not exist"
-  exit 1
+    echo "Target device ${target_device} does not exist"
+    exit 1
 fi
 
 sudo umount ${target_device}1
@@ -114,8 +114,8 @@ sudo umount ${target_device}1
 # Clear the first 32M
 sudo dd if=/dev/zero of=${target_device} bs=1M count=32
 if [ $? != "0" ] ; then
-  echo "erasing failed"
-  exit 1
+    echo "erasing failed"
+    exit 1
 fi
 
 # Make the entire disk a Linux partition
@@ -125,8 +125,8 @@ sudo sfdisk --in-order --Linux --unit M ${target_device} <<-__EOF__
 __EOF__
 
 if [ $? != "0" ] ; then
-  echo "sfdisk failed"
-  exit 1
+    echo "sfdisk failed"
+    exit 1
 fi
 
 sudo mkfs.ext4 ${target_device}1 -L PiggyString
